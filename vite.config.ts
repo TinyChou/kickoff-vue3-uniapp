588926efe1ddfc534e0f853bd4c1fc8a46d39837
TinyChou <zhoujinfu0124@163.com>
2023-03-30 22:58:25 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import eslint from 'vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // do not fail on serve (i.e. local development)
+    {
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+        cache: true,
+        fix: true,
+      }),
+      apply: "serve",
+      enforce: "post",
+    },
+    uni(),
+    vueJsx(),
+  ],
+})
