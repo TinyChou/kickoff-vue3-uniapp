17dea5b19a4d5d3b1251b527d40faa1be6111e97
TinyChou <zhoujinfu0124@163.com>
2023-03-31 14:08:17 +0800

    feat(pinia): add `pinia` support for split application states

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import 'uno.css'
+
+import * as Pinia from 'pinia'
+
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(Pinia.createPinia())
   return {
     app,
+    Pinia,
   }
 }
--- src/pages/uni-ui/test.vue
+++ src/pages/uni-ui/test.vue
@@ -2,7 +2,14 @@
   <uni-badge text="1"></uni-badge>
   <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
   <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+  <text>{{ counter.count }}</text>
 </template>
 <script setup lang="ts">
-function bindClick() {}
+import { useCounterStore } from '@/store/counter'
+
+const counter = useCounterStore()
+function bindClick() {
+  console.log('clicked!', counter.count)
+  counter.increment()
+}
 </script>
