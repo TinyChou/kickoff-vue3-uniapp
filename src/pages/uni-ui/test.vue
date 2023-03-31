4ce6479cd58c7b0812119574880566e8cab4b4c5
TinyChou <zhoujinfu0124@163.com>
2023-03-31 14:16:00 +0800

    chore(deps): add `@vueuse/core` to utilities fns

--- src/pages/uni-ui/test.vue
+++ src/pages/uni-ui/test.vue
@@ -3,13 +3,17 @@
   <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
   <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
   <text>{{ counter.count }}</text>
+  <text>{{ x }}, {{ y }}</text>
 </template>
 <script setup lang="ts">
 import { useCounterStore } from '@/store/counter'
+import { useMouse } from '@vueuse/core'
 
 const counter = useCounterStore()
 function bindClick() {
   console.log('clicked!', counter.count)
   counter.increment()
 }
+
+const { x, y } = useMouse()
 </script>
