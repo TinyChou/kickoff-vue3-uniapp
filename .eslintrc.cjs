20d0dabbcdd03601dfe4224184bb65d2011e7a72
zhoujinfu <859397273@qq.com>
2023-04-02 18:24:56 +0800

    feat(cspell): add `@cspell/eslint-plugin` rules to eslint but comment all rules cause perf issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,20 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
+    // 'plugin:@cspell/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
     'vue/multi-word-component-names': 'off',
+    // '@cspell/spellchecker': [
+    //   'warn',
+    //   {
+    //     checkComments: false,
+    //     autoFix: false,
+    //   }
+    // ],
   },
   globals: {
     uni: true,
